479585311158b81f6185325a44af85731ca9d98b
vigneshraj0329 <vigneshrajstudy@gmail.com>
2023-11-22 19:46:31 +0530

    hero size modified

--- flappy.py
+++ flappy.py
@@ -6,6 +6,10 @@ height=946
 screen=pygame.display.set_mode((width,height))
 bg=pygame.image.load("back_g.png")
 screen.blit(bg,(0,0))
+hero=pygame.image.load("bird.png")
+hero1=pygame.transform.scale(hero,(80,60))
+screen.blit(hero1,(25,450))
+
 pygame.display.update()
 
 time.sleep(5)
